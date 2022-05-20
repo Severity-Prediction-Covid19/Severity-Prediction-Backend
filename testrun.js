febc58402ee4d54b80543468a06d4ece65b8a298
richard <wiryadinatarichard1@gmail.com>
2022-05-20 20:31:45 +0700

    Add Get riwayat and Post riwayat

--- testrun.js
+++ testrun.js
@@ -6,6 +6,12 @@ module.exports = function (app) {
     app.route("/")
         .get(myjson.index);
 
-    app.route("/tambahRiwayat")
+    app.route("/addRiwayat")
         .post(myjson.riwayat);
+
+    app.route("/checkRiwayat/:id")
+        .get(myjson.tampilRiwayat);
+
+    // app.route("/checkRiwayat1/:id")
+    //     .get(myjson.tampilRiwayat1);
 }
